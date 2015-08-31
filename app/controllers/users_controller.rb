74f61c13e1e5b835751b5246d1bce68a99d2b3fa
yosurfer2 <lorenzo.camaione@gmail.com>
2015-08-31 17:51:40 +0200

    Registrations improved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
 	# Never trust parameters from the scary internet, only allow the white list through.
 	def user_params
-		params.require(:user).permit(:email)
+		params.require(:user).permit(:email, :rule)
 	end
 end
